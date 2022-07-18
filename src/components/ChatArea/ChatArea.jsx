e257639877c22f9c418970d45abf67ef3df64b17
chathurperera <chathuraperera007@gmail.com>
2022-07-18 15:59:24 +0530

    chatting area added

--- src/components/ChatArea/ChatArea.jsx
+++ src/components/ChatArea/ChatArea.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import styles from '../ChatArea/ChatArea.module.scss';
+import beginChat from '../../assets/images/icons8-chat.gif'
 const ChatArea = () => {
   return (
-    <div>
-      hwllo
+    <div className={styles.chatArea}>
+      <div className={styles.startChat}>
+        <div className={styles.startChatImage}>
+            <img src={beginChat} alt="" />
+            <div className={styles.startConversation} >Start Conversation </div>
+        </div>
+      </div>
     </div>
   )
 }
