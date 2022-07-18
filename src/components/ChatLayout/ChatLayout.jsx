8d4ed412ac533ceca9a6f4d1c49c46d75c7fcad9
chathurperera <chathuraperera007@gmail.com>
2022-07-18 23:35:22 +0530

    chat window in progress

--- src/components/ChatLayout/ChatLayout.jsx
+++ src/components/ChatLayout/ChatLayout.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "../ChatLayout/ChatLayout.module.scss";
 import profileImage from "../../assets/images/demoPP2.png";
+import video from "../../assets/images/video.png";
+import search from "../../assets/images/search.png";
+import dots from "../../assets/images/dots.png";
+import phone from "../../assets/images/phone.png";
+import sendMessage from "../../assets/images/send-message.png";
+import attachment from "../../assets/images/attachment.png";
+
 const ChatLayout = () => {
   return (
     <div className={styles.chatLayout}>
@@ -10,6 +17,20 @@ const ChatLayout = () => {
           <span className={styles.onlineIndicator}></span>
           <p>Sal Piggee</p>
         </div>
+        <div className={styles.icons}>
+          <img src={phone} alt="phone icon" />
+          <img src={video} alt="video icon" />
+          <img src={search} alt="search icon" />
+          <img src={dots} alt="dots icon" />
+        </div>
+      </div>
+      <div className={styles.chatWindow}>
+        <h1>ss</h1>
+      </div>
+      <div className={styles.messagingArea}>
+        <img className={styles.attachmentIcon} src={attachment} alt="attachment" />
+        <input type="text" name="" placeholder="Enter your message" id="" />
+        <img src={sendMessage} alt="send message icon" />
       </div>
     </div>
   );
