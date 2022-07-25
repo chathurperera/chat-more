7a399907bea8b3593963588b3a37cee7c2451184
chathurperera <chathuraperera007@gmail.com>
2022-07-25 23:15:56 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,18 @@
-import React, { useState } from "react";
-import "../src/App.scss";
-import userProfile from "../src/assets/images/userProfile.jpg";
-import demoPP4 from "../src/assets/images/demoPP4.png";
-import demoPP3 from "../src/assets/images/demoPP3.png";
-import demoPP2 from "../src/assets/images/demoPP2.png";
-import ChatArea from "./components/ChatArea/ChatArea";
-import ChatsList from "./components/ChatsList/ChatsList";
-import ContactList from "./components/ContactList/ContactList";
-import styles from "../src/components/ContactList/ContactList.module.scss";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chats from "./pages/Chats";
+import Login from "./pages/Login";
 
 const App = () => {
-  const [selectedChat, setSelectedChat] = useState({});
-  const [contacts, setContacts] = useState([
-    {
-      name: "Felecia Rower",
-      imageURL: userProfile,
-      bio: "Hi there, im using Chatmore 😉🤞",
-    },
-    {
-      name: "Joaquina Weisenborn",
-      imageURL: demoPP2,
-      bio: "Hi there, im using Chatmore 😉🤞",
-    },
-    {
-      name: "Verla Morgano",
-      imageURL: demoPP3,
-      bio: "Hi there, im using Chatmore 😉🤞",
-    },
-    {
-      name: "Sal Piggee",
-      imageURL: demoPP4,
-      bio: "Hi there, im using Chatmore 😉🤞",
-    },
-  ]);
   return (
-    <main>
-      <aside>
-        <div className={styles.contactListHeader}>
-          <div className={styles.userImage}>
-            <img src={userProfile} alt="user profile" />
-            <div></div>
-          </div>
-          <div className={styles.search}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14px"
-              height="14px"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="#b9b9c3"
-              strokeWidth="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <circle cx="11" cy="11" r="8"></circle>
-              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-            </svg>
-            <input
-              type="text"
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className={styles.searchInput}
-            />
-          </div>
-        </div>
-        <div className="sidebar-container">
-          <ChatsList setSelectedChat={setSelectedChat} />
-          <ContactList contacts={contacts} setSelectedChat={setSelectedChat} />
-        </div>
-      </aside>
-      <div className="chat-area">
-        <ChatArea selectedChat={selectedChat} />
-      </div>
-    </main>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Login.module.scss';
+const Login = () => {
+  return (
+    <main className={styles.login}>
+        <div className={styles.loginLeft}>
+            
+        </div>
+        <div className={styles.loginRight}></div>
+    </main>
+  )
+}
+
+export default Login
