43420c9dea6c137201cf311a17169ff6f7bd6ef9
chathurperera <chathuraperera007@gmail.com>
2022-07-27 01:35:45 +0530

    login form added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import styles from '../styles/Login.module.scss';
+import React from "react";
+import googleIcon from "../assets/images/google.png";
+import styles from "../styles/Login.module.scss";
 const Login = () => {
   return (
     <main className={styles.login}>
-        <div className={styles.loginLeft}>
-            
-        </div>
-        <div className={styles.loginRight}></div>
-    </main>
-  )
-}
+      <div className={styles.loginLeft}>
+        <div className={styles.loginLeftWrapper}>
+          <h4 className={styles.title}>Login</h4>
+          <p className={styles.desc}>
+            Enter your credentials to access your account
+          </p>
+          <button className={styles.loginWithGoogle}>
+            <img src={googleIcon} alt="googleIcon" />
+            Log In with Google
+          </button>
 
-export default Login
+          <div className={styles.divider}>
+            <div></div>
+            <span>or</span>
+            <div></div>
+          </div>
+
+          <form>
+            <div className={styles.inputControl}>
+              <label>Email address</label>
+              <input type="text" required placeholder="name@company.com" />
+            </div>
+            <div className={styles.inputControl}>
+              <label>Password</label>
+              <input type="text" required placeholder="password" />
+            </div>
+            <label htmlFor="rememberLogin" className={styles.rememberLogin}>
+              <input type="checkbox" name="" id="rememberLogin" />
+              Remember information
+            </label>
+            <button className={styles.loginButton}>Login</button>
+          </form>
+        </div>
+      </div>
+      <div className={styles.loginRight}></div>
+    </main>
+  );
+};
+
+export default Login;
